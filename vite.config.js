f80f924f0eefa54cb77468250303187c3c5bb5ad
Billy Castro <billycastro.1822@gmail.com>
2024-09-30 00:46:02 -0500

    vite.config actualizado

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/trabajos-insti-apps/', // Reemplaza <REPO> con el nombre de tu repositorio
+  base: 'https://billycastrob.github.io/trabajos-insti-apps', // Reemplaza <REPO> con el nombre de tu repositorio
   build: {
     outDir: 'dist',
   },

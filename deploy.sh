c1d1186b01825ce47d1a2703c43b533d8edecd7b
Billy Castro <billycastro.1822@gmail.com>
2024-09-29 23:54:20 -0500

    deploy.sh actualizado

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abortar en caso de error
+set -e
+
+# compilar el proyecto
+npm run build
+
+# navegar a la carpeta de salida de compilación
+cd dist
+
+# inicializar un nuevo repositorio git
+git init
+git add -A
+git commit -m 'deploy'
+
+# si estás desplegando en https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# si estás desplegando en https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:BillyCastroB/trabajos-appsweb-moviles.git main:gh-pages
+
+# volver al directorio anterior
+cd -

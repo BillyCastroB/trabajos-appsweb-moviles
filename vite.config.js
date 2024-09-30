289d83acf9db04b8b99e8da8c28479a1b6e09fe1
Billy Castro <billycastro.1822@gmail.com>
2024-09-30 00:13:15 -0500

    gitignore actualizado

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/trabajos-appsweb-moviles/', // Reemplaza <REPO> con el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+  },
 })

f7b49c7f7fadefba7dd6748e218818ac6d022f1f
Billy Castro <billycastro.1822@gmail.com>
2024-09-29 22:41:58 -0500

    enlaces del main y nombres ahora funcionales

--- src/components/Main/Item/Item.jsx
+++ src/components/Main/Item/Item.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Item.css';
-export const Item = () => {
+export const Item = ({enlace, semana}) => {
   return (
     <div className='item'>
         <div className='item--icono'>
             <img src="/images/check-icono.png" alt="icono" />
         </div>
         <div className='item--descripcion'>
-            <h2>número de semama</h2>
+            <h2>{semana}</h2>
             <p>Breve descripcion</p>
-            <a className='enlace-github' href="">Enlace a github</a>
+            <a className='enlace-github' href={enlace} >Enlace a github</a>
         </div>
     </div>
   )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,21 +7,55 @@ export const Main = () => {
       <div className='contenedor-main'>
         <h2 className='trabajos--titulo'>Trabajos de la semana</h2>
         <div className='grid__main'>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
+          <Item
+            semana={"Semana #1"}
+          />
+          <Item
+            semana={"Semana #2"}
+            enlace={"https://github.com/BillyCastroB/archivos-tareas/blob/main/Actividad%2002%20CastroBullon.pdf"}
+          />
+          <Item
+            semana={"Semana #3"}
+            enlace={"https://github.com/BillyCastroB/archivos-tareas/blob/main/cuadrado.dart"}
+          />
+          <Item
+            semana={"Semana #4"}
+          />
+          <Item
+            semana={"Semana #5"}
+          />
+          <Item
+            semana={"Semana #6"}
+          />
+          <Item
+            semana={"Semana #7"}
+          />
+          <Item
+            semana={"Semana #8"}
+          />
+          <Item
+            semana={"Semana #9"}
+          />
+          <Item
+            semana={"Semana #10"}
+          />
+          <Item
+            semana={"Semana #11"}
+          />
+          <Item
+            semana={"Semana #12"}
+          />
+          <Item
+            semana={"Semana #13"}
+          />
+          <Item
+            semana={"Semana #14"}
+          />
+          <Item
+            semana={"Semana #15"}
+          />
+
+        
         </div>
       </div>
     </main>
